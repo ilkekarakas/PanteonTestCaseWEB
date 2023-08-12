f3d5f6c91888fdd7a380dd8ccb36e202d046a9eb
İlke <h.ilke500@hotmail.com>
2023-08-13 01:34:13 +0300

    Update BuildingConfigurationPage.js

--- src/components/Pages/BuildingConfigurationPage.js
+++ src/components/Pages/BuildingConfigurationPage.js
@@ -82,7 +82,7 @@ function BuildingConfigurationPage() {
     }
   };
 
-  const baseURL = "http://16.171.27.88/api"; // Base URL'nin sonunda / olmamasına dikkat edin
+  const baseURL = "https://16.171.27.88/api"; // Base URL'nin sonunda / olmamasına dikkat edin
 
   const httpClient = axios.create({
     baseURL
